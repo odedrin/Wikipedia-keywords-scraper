21918915228785914fcdf2ab113967753d3fcd33
Oded Rinsky <Oded@Odeds-MacBook-Pro.local>
2020-09-01 15:36:17 +0300

    Bugs fixed, version working

--- part1.py
+++ part1.py
@@ -1,15 +1,15 @@
 import re
-from timeit import default_timer as timer
 import json
 
 class SearchEngine():
     def __init__(self, config):
-        self.keywords = self._config_keywords(config['keywords_file'])
-        self.patterns = self._config_patterns(self.keywords)
+        self._translate_table = { ord('('): '\\(', ord(')'): '\\)' }
+        self._keywords = self._config_keywords(config['keywords file'])
+        self._patterns = self._config_patterns(self._keywords)
 
     def search(self, text):
         keyword_set = set()
-        for pattern in self.patterns:
+        for pattern in self._patterns:
             pattern = re.compile(pattern, re.I)
             match = re.search(pattern, text)
             if match:
@@ -19,7 +19,7 @@ class SearchEngine():
     def _config_keywords(self, keywords_file):
         try:
             with open(keywords_file, 'r') as f:
-                return f.read().replace('(', '\(').replace(')', '\)').split('\n')
+                return f.read().split('\n')
         except IOError:
             print('Invalid filename. aborting.')
             exit()
@@ -28,7 +28,7 @@ class SearchEngine():
         patterns = []
         for keyword in keywords:
             splitted_keyword = re.split(r'[- ]', keyword)
-            pattern = r'\b(' + r'[ -]?'.join(splitted_keyword) + r')\b'
+            pattern = r'\b(' + r'[ -]?'.join(splitted_keyword).translate(self._translate_table) + r')(?!\w)'
             patterns.append(pattern)
         return patterns
 
@@ -47,15 +47,8 @@ class Configuration():
 
 if __name__ == '__main__':
     config = Configuration('config_file.json')
-    engine = SearchEngine(config)
-    print(engine.patterns)
-
-    # start = timer()
-    # print(engine.search('Welcome to >>GENERAL-motors! We love programming! We are also in the Networks Industrial buisness'))
-    # end = timer()
-    # print(end - start)
+    searchengine = SearchEngine(config)
+    text = 'INSERT TEXT HERE'
+    word_found_in_text = searchengine.search(text)
+    print(word_found_in_text)
     
-    # start = timer()
-    # print(engine.search('Welcome to >>GENERAL-motors! We love programming! We are also in the Networks (Industrial) buisness'))
-    # end = timer()
-    # print(end - start)
--- part2.py
+++ part2.py
@@ -1,25 +1,22 @@
 import threading
 import time
-import random
-from datetime import datetime
 from urllib.request import Request, urlopen, HTTPError, URLError
 from part1 import SearchEngine, Configuration
-import json
 
 #The following class reads the configuration file, 
 class ScraperManager():
     def __init__(self, config):
-        self.semaphore = threading.BoundedSemaphore(value=config['rps'])
-        self.workers = []
+        self.semaphore = threading.BoundedSemaphore(value=config['requests per second'])
+        self._workers = []
         self._init_workers()
         self.websearchengine = WebSearchEngine(config)
 
     def scrape(self):
-        for worker in self.workers:
+        for worker in self._workers:
             worker.start()
         time.sleep(1)
         while True:
-            for _ in range(config['rps']):
+            for _ in range(config['requests per second']):
                 try:
                     self.semaphore.release()
                 except ValueError as v:
@@ -27,17 +24,17 @@ class ScraperManager():
             time.sleep(1)
 
     def _init_workers(self):
-        for i in range(config['num_of_workers']):
-            self.workers.append(MyWorker(target=ScraperManager._work, args=(self,), name='{}'.format(i+1)))
+        for i in range(config['number of workers']):
+            self.workers.append(MyWorker(target=ScraperManager.work, args=(self,), name='{}'.format(i+1)))
 
     @staticmethod
-    def _work(self, manager):
+    def work(self, manager):
         current_thread = threading.current_thread()
         worker_number = current_thread.name
         while True:
             manager._request_permission_to_work()
             matches, url = manager.websearchengine.fetch_and_search()
-            message = 'Worker: {}\nRandom URL: {}\nMatches: {}\n-----------------------------\n'.format(worker_number,url,matches)
+            message = 'Worker: {}\nRandom URL: {}\nMatches: {}\n-----------------------------'.format(worker_number,url,matches)
             print(message)
 
     def _request_permission_to_work(self):
@@ -45,7 +42,7 @@ class ScraperManager():
 
 class MyWorker(threading.Thread):
     def __init__(self, group=None, target=None, name=None, args=(), kwargs=None):
-        super().__init__(group=group, target=target, name=name)
+        super().__init__(group=group, target=target, name=name, daemon=True)
         self.target = target
         self.args = args
         return
@@ -58,19 +55,19 @@ class WebSearchEngine(SearchEngine):
         SearchEngine.__init__(self, config)
         
     def fetch_and_search(self):
-        text, actual_url = self._get_data(config['url_gen'])
-        found = SearchEngine.search(self, text) #check what happens if url is invalid
-        return found, actual_url
+        text, actual_url = self._fetch_data(config['url generator'])
+        keywords_found = SearchEngine.search(self, text) 
+        return keywords_found, actual_url
 
-    def _get_data(self, url):
+    def _fetch_data(self, url):
         try:
-            request = Request(url)
-            with urlopen(request) as response:
+            with urlopen(Request(url)) as response:
                 html = response.read()
                 actual_url = response.geturl()
                 return str(html, encoding='utf-8'), actual_url
         except (URLError, HTTPError):
-            print('Cannot open url: {}'.format(url))
+            print('Cannot open url: {}. Aborting'.format(url))
+            exit()
 
 if __name__ == '__main__':
     config = Configuration('config_file.json')
